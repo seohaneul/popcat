1366e4cfcddf6490c5c191c4890c824fdeff531a
seohaneul <seo1533618@gmail.com>
2023-05-07 17:04:44 +0900

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { isBrowser } from "react-device-detect";
+import Login from './page/Login';
+import NotFound from './page/NotFound';
+import Pc from './page/Pc';
+import Pop from './page/Pop';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isBrowser ? 
+      (<BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Pc/>}/>
+      <Route path="/pop" element={<Pc/>}/>
+      <Route path="*" element={<NotFound/>}/>
+      </Routes>
+      </BrowserRouter>) 
+      : 
+      (<BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Login/>}/>
+      <Route path="/pop" element={<Pop/>}/>
+      <Route path="*" element={<NotFound/>}/>
+      </Routes>
+      </BrowserRouter>)}
+      
     </div>
   );
 }
---
+++ src/page/Pop.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Pop() {
+  return (
+    <div>Pop</div>
+  )
+}
